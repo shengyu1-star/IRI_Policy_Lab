e3a832db9aa04f3613d7be62c238ab529bc74ab8
sean2251 <smcsweeney@uchicago.edu>
2022-04-21 21:06:16 -0500

    reconciled variables, added codebook

--- creating country_year elections.R
+++ creating country_year elections.R
@@ -1,4 +1,12 @@
 library(tidyverse)
+library(remotes)
+#remotes::install_github("xmarquez/democracyData") #need this package to download FH data
+library(democracyData)
+library(WDI)
+library(countrycode)
+
+#fh <- download_fh(verbose = FALSE) #uncomment these for first time running program
+#polity5 <- download_polity_annual(verbose = FALSE)
 
 setwd("~/Documents/School/Grad/Spring 2022/IRI Policy Lab/IRI_Policy_Lab")
 
@@ -16,8 +24,9 @@ PEI_election <- read_delim("Raw data/PEI election-level data (PEI_7.0) v2 09-05-
                            trim_ws = TRUE)
 
 # IRI countries
-iri_countries <- c("Kenya", "Chad", "Somaliland", "Somalia", "Sudan", "Nigeria", 
+iri_countries <- c("Kenya", "Chad", "Somalia", "Sudan", "Nigeria", 
                    "Sierra Leone", "Congo", "Zimbabwe")
+iri_countries_ios2 <- countrycode(iri_countries, origin = 'country.name', destination = 'iso2c')
 
 # other country groups
 all_africa <- c()
@@ -156,6 +165,98 @@ IAEP_QED_PEI <- IAEP_QED %>%
   arrange(country_year)
 
 #exporting IAEP_QED_PEI
-IAEP_QED_PEI %>% write_csv("CleanedMergedData/IAEP_QED_PEI_clean.csv")
+#IAEP_QED_PEI %>% write_csv("CleanedMergedData/IAEP_QED_PEI_clean.csv")
 
+###-----------------------------####
+# Reconciling and consolidating variables in IAEP_QED_PEI
+# variables reconciled so far: post_elec_violence, turnout, elec_integrity, pre_elec_legal_integrity, elec_explicit_cheating
+# variables still to be reconciled: #_years_since_last_regime_change, FH_score, single-party
+
+IAEP_QED_PEI_reconciled <- IAEP_QED_PEI %>% 
+  mutate(post_elec_violence = case_when(`Voting results/reactions index (protests/disputes) (0-100), imputed` > 0 & `Voting results/reactions index (protests/disputes) (0-100), imputed` <= 25 ~ 3,
+                                        `protestpart` == "Widespread participation" ~ 3,
+                                        `Election day violence/unrest` == "(3) High - major problems" ~ 3,
+                                        `Voting results/reactions index (protests/disputes) (0-100), imputed` > 25 & `Voting results/reactions index (protests/disputes) (0-100), imputed` <= 50 ~ 2,
+                                        `protestpart` == "Moderate participation" ~ 2,
+                                        `Election day violence/unrest` == "(2) Moderate - moderate problems" ~ 2,
+                                        `Voting results/reactions index (protests/disputes) (0-100), imputed` > 50 & `Voting results/reactions index (protests/disputes) (0-100), imputed` <= 75 ~ 1,
+                                        `protestpart` == "Low participation" ~ 1,
+                                        `Election day violence/unrest` == "(1) Low - minor problems only" ~ 1,
+                                        `Voting results/reactions index (protests/disputes) (0-100), imputed` > 75 & `Voting results/reactions index (protests/disputes) (0-100), imputed` <= 100 ~ 0,
+                                        `Did election cause protest/violence?` == "No" ~ 0,
+                                        `Election day violence/unrest` == "(0) Good - no problems" ~ 0,
+                                        TRUE ~ NA_real_),
+         `% of pop voting in leg elec` = as.numeric(ifelse(`% of pop voting in leg elec` == ".a", NA_real_, `% of pop voting in leg elec`)),
+         `% of pop voting in exec elec` = as.numeric(ifelse(`% of pop voting in exec elec` == ".a", NA_real_, `% of pop voting in exec elec`))) %>% 
+  rowwise() %>% 
+  mutate(turnout = mean(c(Turnout, 
+                          `% of pop voting in leg elec`, 
+                          `% of pop voting in exec elec`), na.rm = TRUE)) %>% 
+  mutate(elec_integrity = case_when(`Extent of election problems` == "(3) High - major problems" ~ 3,
+                                    `Extent of election problems` == "(2) Moderate - moderate problems" ~ 2,
+                                    `Extent of election problems` == "(1) Low - minor problems only" ~ 1,
+                                    `Extent of election problems` == "(0) Good - no problems" ~ 0,
+                                    `PEI index of electoral integrity, imputed` > 60 ~ 0,
+                                    `PEI index of electoral integrity, imputed` > 50 & `PEI index of electoral integrity, imputed` <= 60 ~ 1,
+                                    `PEI index of electoral integrity, imputed` > 40 & `PEI index of electoral integrity, imputed` <= 50 ~ 2,
+                                    `PEI index of electoral integrity, imputed` <=40 ~ 3)) %>% 
+  mutate(pre_elec_legal_integrity = case_when(`Pre-election legal structural enviornment` == "(3) High - major problems" ~ 3,
+                                              `Pre-election legal structural enviornment` == "(2) Moderate - moderate problems" ~ 2,
+                                              `Pre-election legal structural enviornment` == "(1) Low - minor problems only" ~ 1,
+                                              `Pre-election legal structural enviornment` == "(0) Good - no problems" ~ 0,
+                                              `Electoral laws index (0-100), imputed` > 60 ~ 0,
+                                              `Electoral laws index (0-100), imputed` > 50 & `Electoral laws index (0-100), imputed` <= 60 ~ 1,
+                                              `Electoral laws index (0-100), imputed` > 40 & `Electoral laws index (0-100), imputed` <= 50 ~ 2,
+                                              `Electoral laws index (0-100), imputed` <=40 ~ 3)) %>% 
+  mutate(elec_explicit_cheating = case_when(`Election day explicit cheating` == "(3) High - major problems" ~ 3,
+                                              `Election day explicit cheating` == "(2) Moderate - moderate problems" ~ 2,
+                                              `Election day explicit cheating` == "(1) Low - minor problems only" ~ 1,
+                                              `Election day explicit cheating` == "(0) Good - no problems" ~ 0,
+                                              `Vote count index (0-100), imputed` > 60 ~ 0,
+                                              `Vote count index (0-100), imputed` > 50 & `Vote count index (0-100), imputed` <= 60 ~ 1,
+                                              `Vote count index (0-100), imputed` > 40 & `Vote count index (0-100), imputed` <= 50 ~ 2,
+                                              `Vote count index (0-100), imputed` <=40 ~ 3)) %>% 
+  select(cname, year.x, country_year, electexec, electleg, electboth, elec_integrity, post_elec_violence, turnout, 
+         pre_elec_legal_integrity, elec_explicit_cheating, govstruct, `Leg elec sys`)
+
+
+## FH & Polity5 ##########
+
+polity5_mod <- polity5 %>% 
+  filter(polity_annual_country %in% iri_countries) %>% 
+  mutate(country_year = paste(polity_annual_country, year, sep = "-")) %>%
+  select(country_year, fragment, polity2, durable, parreg, parcomp, regtrans)
+
+fh_mod <- fh %>% 
+  filter(fh_country %in% iri_countries) %>% 
+  mutate(country_year = paste(fh_country, year, sep = "-")) %>% 
+  select(country_year, status, fh_total)
+
+polity5_fh <- polity5_mod %>% 
+  full_join(fh_mod, by = "country_year")
+
+## IAEP_QED_PED_reconciled + fh_polity5
+IAEP_QED_PEI_FH_POL5 <- IAEP_QED_PEI_reconciled %>% 
+  left_join(polity5_fh, by = "country_year")
+
+# World Bank Indicators (WDI)
+
+iri_WDI <- WDI(country = iri_countries_ios2, indicator = c("NY.GDP.PCAP.KD", 
+                                                           "BX.KLT.DINV.CD.WD",
+                                                           "FP.CPI.TOTL.ZG",
+                                                           "SI.POV.GINI"))
+iri_WDI <- iri_WDI %>% 
+  rename("GDP per capita (constant 2015 US$)" = "NY.GDP.PCAP.KD",
+         "Foreign direct investment, net inflows (BoP, current US$" = "BX.KLT.DINV.CD.WD",
+         "Inflation, consumer prices (annual %)" = "FP.CPI.TOTL.ZG",
+         "Gini index" = "SI.POV.GINI") %>%
+  mutate(country = ifelse(country == "Congo, Rep.", "Congo", country),
+         country_year = paste(country, year, sep = "-")) %>% 
+  select(!c(iso2c, country, year))
+
+## IAEP_QED_PED_reconciled + fh_polity5
+IAEP_QED_PEI_FH_POL5_WB <- IAEP_QED_PEI_FH_POL5 %>% 
+  left_join(iri_WDI, by = "country_year")
+
+IAEP_QED_PEI_FH_POL5_WB %>% write_csv("CleanedMergedData/IAEP_QED_PEI_FH_POL5_WB_clean.csv")
 
